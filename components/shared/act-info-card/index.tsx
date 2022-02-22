4f19193e08bb40013c8c93f754fef9ab7dc5cafa
universess <universess@qq.com>
2022-02-22 16:51:29 +0800

    💩  feat/ add act-info-card

--- components/shared/act-info-card/index.tsx
+++ components/shared/act-info-card/index.tsx
@@ -1,34 +1,97 @@
-import React from "react";
-import styles from "./index.module.scss"
+import React, { useState } from "react";
+import { DatePicker, Toast } from "antd-mobile";
+import styles from "./index.module.scss";
 
-const ActInfoCard: React.FC = () => {
-  return(
+interface ActInfoCardProps {
+  isOnModify?: boolean;
+}
+
+const ActInfoCard: React.FC<ActInfoCardProps> = (props: ActInfoCardProps) => {
+  const [visible1, setVisible1] = useState(false);
+  const [visible2, setVisible2] = useState(false);
+
+  return (
     <div className={styles.card_text_div}>
-        <div className={styles.card_text_title}>
-          南昌大学百年医学教育"南昌大学百年医学教育"南昌大学百年医学教育
-        </div>
-        <div className={styles.card_text_info}>
-          <div style={{ marginBottom: "10px" }} className={styles.info_detail}>
-            <img
-              style={{ width: "15px", marginRight: "10px" }}
-              src={"/img/location.svg"}
-            />
-            <span style={{ fontSize: "5px", color: "#707070" }}>
+      <div className={styles.card_text_title}>
+        {props.isOnModify ? (
+          <textarea
+            className={styles.title_input}
+            defaultValue={`南昌大学百年医学教育“医教协同创新发展”高峰论坛（医学研究分论坛）`}
+          ></textarea>
+        ) : (
+          <span>
+            南昌大学百年医学教育“医教协同创新发展”高峰论坛（医学研究分论坛）
+          </span>
+        )}
+      </div>
+      <div className={styles.card_text_info}>
+        <div style={{ marginBottom: "20px" }} className={styles.info_detail}>
+          <img
+            style={{ width: "15px", marginRight: "10px" }}
+            src={"/img/location.svg"}
+          />
+          {props.isOnModify ? (
+            <textarea
+              className={styles.location_input}
+              defaultValue={`南昌大学玛丽女王学院报告厅`}
+            ></textarea>
+          ) : (
+            <span style={{ fontSize: "3.7vw", color: "#707070" }}>
               南昌大学玛丽女王学院报告厅
             </span>
-          </div>
-          <div style={{ paddingBottom: "20px" }} className={styles.info_detail}>
-            <img
-              style={{ width: "15px", marginRight: "10px" }}
-              src={"/img/calender.svg"}
-            />
-            <span style={{ fontSize: "5px", color: "#707070" }}>
+          )}
+        </div>
+        <div style={{ paddingBottom: "20px" }} className={styles.info_detail}>
+          <img
+            style={{ width: "15px", marginRight: "10px" }}
+            src={"/img/calender.svg"}
+          />
+          {props.isOnModify ? (
+            <div>
+              <span
+                onClick={() => {
+                  setVisible1(true);
+                }}
+              >
+                开始时间 --- --{" "}
+              </span>
+              <DatePicker
+                visible={visible1}
+                onClose={() => {
+                  setVisible1(false);
+                }}
+                precision="month"
+                onConfirm={(val) => {
+                  Toast.show(val.toString());
+                }}
+              />
+              <span
+                onClick={() => {
+                  setVisible2(true);
+                }}
+              >
+                结束时间 -- -- --
+              </span>
+              <DatePicker
+                visible={visible2}
+                onClose={() => {
+                  setVisible2(false);
+                }}
+                precision="minute"
+                onConfirm={(val) => {
+                  Toast.show(val.toString());
+                }}
+              />
+            </div>
+          ) : (
+            <span style={{ fontSize: "3.7vw", color: "#707070" }}>
               7/8 16:00 - 7/8 19:00
             </span>
-          </div>
+          )}
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default ActInfoCard;
--- pages/ActDetail/index.tsx
+++ pages/ActDetail/index.tsx
@@ -7,32 +7,7 @@ const ActDetail: React.FC = () => {
   return (
     <div className={styles.background}>
       <HeadBar />
-      {/* <div className={styles.card_text_div}>
-        <div className={styles.card_text_title}>
-          南昌大学百年医学教育"南昌大学百年医学教育"南昌大学百年医学教育
-        </div>
-        <div className={styles.card_text_info}>
-          <div style={{ marginBottom: "10px" }} className={styles.info_detail}>
-            <img
-              style={{ width: "15px", marginRight: "10px" }}
-              src={"/img/location.svg"}
-            />
-            <span style={{ fontSize: "5px", color: "#707070" }}>
-              南昌大学玛丽女王学院报告厅
-            </span>
-          </div>
-          <div style={{ paddingBottom: "20px" }} className={styles.info_detail}>
-            <img
-              style={{ width: "15px", marginRight: "10px" }}
-              src={"/img/calender.svg"}
-            />
-            <span style={{ fontSize: "5px", color: "#707070" }}>
-              7/8 16:00 - 7/8 19:00
-            </span>
-          </div>
-        </div>
-      </div> */}
-      <ActInfoCard/>
+      <ActInfoCard isOnModify={true} />
       <div className={styles.cut_line}>发布者</div>
       <div className={styles.author}>
         <img
