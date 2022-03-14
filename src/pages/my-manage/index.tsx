2f0f5b42dd2f3e8ff3178a58e3cf75cd98d9e7ad
universess <universess@qq.com>
2022-03-14 23:18:21 +0800

    feat: 增加社团账号激活状态不同样式表现

--- src/pages/my-manage/index.tsx
+++ src/pages/my-manage/index.tsx
@@ -4,20 +4,29 @@ import HeadBar from "@/components/shared/head-bar";
 import styles from "./index.module.scss";
 
 const MyManage: React.FC = () => {
-  const isActivate: boolean = true;
+  const isActivated: boolean = false;
 
   return (
     <div className={styles.background}>
       <HeadBar />
       <div className={styles.card_list}>
-        <div className={styles.account_card}>
-          <img src={"/img/img.svg"} />
-          <span>家园工作室</span>
-          {/* <span>{isActivate ? "" }</span> */}
-          <Link to="/my-manage/amend-team" replace>
-            {isActivate ? <span>编辑</span> : <span>激活</span>}
-          </Link>
-        </div>
+        {isActivated ? (
+          <div className={styles.account_card_active}>
+            <img src={"/img/img.svg"} />
+            <span>家园工作室</span>
+            <Link to="/my-manage/amend-team" replace>
+              <span>编辑</span>
+            </Link>
+          </div>
+        ) : (
+          <div className={styles.account_card_inactive}>
+            <img src={"/img/img.svg"} />
+            <span>家园工作室</span>
+            <Link to="/my-manage/amend-team" replace>
+              <span>激活</span>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
